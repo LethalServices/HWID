654dcf9e83b02d429b1776c2d6d7c170d2d42342
Joshua <73680704+UrFingPoor@users.noreply.github.com>
2023-05-03 19:28:42 -0400

    Update 1.9.3
    - Removed The Download For Git
    - Updated API Links
    - Updated Version From 1.9 - 1.9.3

--- hwid.py
+++ hwid.py
@@ -13,8 +13,7 @@ print(Fore.LIGHTMAGENTA_EX+'     ██║     ██╔══╝     ██║ 
 print(Fore.LIGHTMAGENTA_EX+'     ███████╗███████╗   ██║   ██║  ██║██║  ██║███████╗     '.center(width))
 print(Fore.LIGHTMAGENTA_EX+'     ╚══════╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝     '.center(width))
 print(Fore.LIGHTCYAN_EX+'        TOS: Lethal Holds No Responsibility At ALL!         '.center(width))
-print(Fore.WHITE+f'           Version: Beta 1.9 | https://lethal.ml            '.center(width))
-
+print(Fore.WHITE+f'           Version: Beta 1.9.3 | https://lethals.org            '.center(width))
 
 def get_download(url, filename):
     with requests.get(url, stream=True) as r:
@@ -35,27 +34,14 @@ def get_download_json(url, filename):
                     if data:  
                         f.write(data)
                         bar.update(len(data)) 
-
-                   
+                 
 def get_discord_settings_json():
     replacesetting = input(f'\n\n[{datetime.now().strftime("%H:%M:%S")}] [{Fore.GREEN}info{Fore.WHITE}] [+] Would you like to enable LCTRL + LSHIFT + I for discord? (y/n): ')
     if replacesetting == 'y':
         if os.path.isfile(f"{os.getenv('APPDATA')}\\discord\\settings.json"):
             os.remove(f"{os.getenv('APPDATA')}\\discord\\settings.json")                 
-            get_download_json('https://lethal.ml/settings', 'settings.json')
+            get_download_json('https://lethals.org/settings', 'settings.json')
             print(f'[{datetime.now().strftime("%H:%M:%S")}] [{Fore.GREEN}Info{Fore.WHITE}] [+] Enabled LCTRL + LSHIFT + I, Restart Discord')
-    else:
-        pass
-
-def get_git_for_windows():
-    installgit = input(f'\n\n[{datetime.now().strftime("%H:%M:%S")}] [{Fore.GREEN}info{Fore.WHITE}] [+] Would you like to install git for windows? (y/n): ')
-    if installgit == 'y':
-        if ctypes.sizeof(ctypes.c_voidp) == 4:
-            get_download('https://github.com/git-for-windows/git/releases/download/v2.39.1.windows.1/Git-2.39.1-32-bit.exe', 'Git-2.39.1-32-bit.exe')
-            os.system(f"{os.getcwd()}\\Git-2.39.1-32-bit.exe")
-        else:       
-            get_download('https://github.com/git-for-windows/git/releases/download/v2.39.1.windows.1/Git-2.39.1-64-bit.exe', 'Git-2.39.1-64-bit.exe')
-            os.system(f"{os.getcwd()}\\Git-2.39.1-64-bit.exe")
     else:
         pass
 
@@ -64,10 +50,8 @@ def get_hwid():
     print(f'[{datetime.now().strftime("%H:%M:%S")}] [{Fore.GREEN}info{Fore.WHITE}] [+] Copy & Paste Your HWID Into The Ticket\\Bot Command Field')
     Exit=input(f'[{datetime.now().strftime("%H:%M:%S")}] [{Fore.GREEN}info{Fore.WHITE}] [+] Press any key to exit')
 
-
 def main():
     get_discord_settings_json()
-    get_git_for_windows()
     get_hwid()
 
 if __name__ == "__main__":
